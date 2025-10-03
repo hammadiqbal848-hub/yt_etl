04dfcb4677b23b48424d8a68d946023bd7be8c51
hammadiqbal848 <hammadiqbal848@gmail.com>
2025-10-03 07:10:14 +0200

    saving data to json func and data json

--- video_stats.py
+++ video_stats.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from dotenv import load_dotenv
+from datetime import date
 import os
 
 load_dotenv()
@@ -84,8 +85,18 @@ def extract_video_data(video_ids):
     return video_data
 
 
+def save_to_json(extracted_data):
+    
+    file_path = f'./data/yt_data_{date.today()}.json'
+    
+    with open(file_path, 'w', encoding='utf=8') as json_file:
+        json.dump(extracted_data, json_file, indent=4, ensure_ascii=False)
+
+
+
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
     video_data = extract_video_data(video_ids)
-    print(f"Extracted data for {len(video_data)} videos.")
+    save_to_json(video_data)
